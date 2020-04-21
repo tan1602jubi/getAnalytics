d36ffcfae024047d38344c3f2eefd3ee90dd92f1
tan1602jubi <tanmay@jubi.ai>
2020-04-21 18:15:12 +0530

    msg

---
+++ create.js
@@ -0,0 +1,15 @@
+require("parramato").createFrontend({
+    root:"http://152.67.1.65/get_analytics",
+    staticDirectory:__dirname+"/static",
+    projectId:"get_analytics_236145124787",
+    passphraseMiddleware:"YGUYGgyjgblgUGIYGIGkwhbiuashbo98u9283hr9h24rqIYGI932kbidbiadsYE",
+    firebaseWebConfig:{
+        apiKey: "AIzaSyAAqVdFN_8wbXK4W_YLZj2q6rF-ZrO9xKQ",
+        authDomain: "push-notification-f31b9.firebaseapp.com",
+        databaseURL: "https://push-notification-f31b9.firebaseio.com",
+        projectId: "push-notification-f31b9",
+        storageBucket: "",
+        messagingSenderId: "417522185572"
+    },
+    iconPath:"/icon.png"
+ })
---
+++ index.js
@@ -0,0 +1,27 @@
+require("parramato").Server({
+    root:"http://152.67.1.65/get_analytics",
+    socketLocalPath: '/socket',
+    httpPort:7000,
+    cluster:false,
+    dbUri:'mongodb://getanalyticsdb:analyticsdb123@127.0.0.1:27017/analytics',
+    staticDirectory:__dirname+"/static",
+    adapterPath:"/adapter",
+    adapterDirectory:__dirname+"/adapter",
+    projectId:"get_analytics_236145124787",
+    dashbotKey:"VxtYPVW6168LIiXwqpIku9wE",
+    directMultiplier:1,
+    fallbackMultiplier:0.8,
+    passphraseMiddleware:"YGUYGgyjgblgUGIYGIGkwhbiuashbo98u9283hr9h24rqIYGI932kbidbiadsYE",
+    timeoutSeconds:60,
+    fcmServerKey:"AAAAYTZC9WQ:APA91bFRmKa",
+    firebaseWebConfig:{
+        apiKey: "sd-ZrO9xKQ",
+        authDomain: "on-f31.firebaseapp.com",
+        databaseURL: "https://on-f31.firebaseio.com",
+        projectId: "on-f31",
+        storageBucket: "",
+        messagingSenderId: "4175221234234"
+    }
+},()=>{
+    //TO DO AFTER INITIALIZATION
+})
