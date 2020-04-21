7209e713ba29d6b1f225232c7cf9615382c8c4aa
tan1602jubi <tanmay@jubi.ai>
2020-04-21 20:33:27 +0530

    msg

---
+++ adapter/intent/getinfo.py
@@ -0,0 +1,20 @@
+import pymongo
+m = pymongo.MongoClient('mongodb+srv://sharedParaRead:k07r4FDUpSedawJ6@sharedparramato-sy1qy.mongodb.net/test?retryWrites=true&w=majority')
+db = m.get_database('shared-parramato')
+
+
+def getPids(id):
+    p = list(db.projectMapper.find())
+    pids = [i["projectId"] for i in p]
+    if id in pids:
+        return "1"
+    else:
+        return "0" 
+
+if __name__ == "__main__":
+
+    data = json.loads(sys.argv[1])
+    if data["operation"] == "chkId":
+        print(getPids(data["id"]))
+
+
--- adapter/intent/post.js
+++ adapter/intent/post.js
@@ -1,19 +1,39 @@
+let {PythonShell} = require('python-shell')
 module.exports = {
 	getProjectId: (model) => {
 		return new Promise(async function (resolve, reject) {
 			console.log(model.data)
-			resolve(model)
+			let options = {
+				mode: 'text',
+				pythonOptions: ['-u'],
+				scriptPath: './getinfo.py',//Path to your script
+				args: [JSON.stringify({"operation": "chkId", "id": model.data})]
+			};
+	  
+			await PythonShell.run('getinfo.py', options, function (err, results) {
+				if (err) throw err;
+				console.log('results: ');
+				if (results[0] == "1"){
+					delete(model.stage)
+					resolve(model)
+				}
+				else{
+					reject(model)
+				}
+			})
 		})
 	},
 	firstMessage : (model) => {
 		return new Promise(async function (resolve, reject) {
 			console.log(model.data)
+			delete(model.stage)
 			resolve(model)
 		})
 	},
 	fallbackMessage: (model) => {
 		return new Promise(async function (resolve, reject) {
 			console.log(model.data)
+			delete(model.stage)
 			resolve(model)
 		})
 	},
